faef81f5f65699f0f9d9e13a417b31ef8ea86dd0
chanokin <chanokin@gmail.com>
2018-11-22 10:41:47 +0000

    added minimal test script v1

--- codebase/portal_test.py
+++ codebase/portal_test.py
@@ -6,8 +6,11 @@ marocco.backend = PyMarocco.Hardware
 
 pynn.setup(marocco=marocco)
 
-neuron = pynn.Population(1, pynn.IF_cond_exp, {})
+neurons = pynn.Population(1, pynn.IF_cond_exp, {})
 inputs = pynn.Population(1, sim.SpikeSourcePoisson(rate=10.0))
+
+proj = pynn.Projection(neurons, inputs, 
+        pynn.OneToOneConnector(5.0))
 
 pynn.run(10)
 
