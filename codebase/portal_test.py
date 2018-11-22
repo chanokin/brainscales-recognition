efe06c642375ba82333b568fed4e214e9d4516cb
chanokin <chanokin@gmail.com>
2018-11-22 15:47:02 +0000

    minimal test for ESS - need to add portal support

--- codebase/portal_test.py
+++ codebase/portal_test.py
@@ -1,18 +1,35 @@
-import pyhmf as pynn
-from pymarocco import PyMarocco
+import matplotlib 
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
 
-marocco = PyMarocco()
-marocco.backend = PyMarocco.Hardware
+import pyNN.hardware.brainscales as pynn
+#import pyNN.hardware.facets as pynn
 
-pynn.setup(marocco=marocco)
+USE_EMU = bool(1)
+pynn.setup(useSystemSim=USE_EMU)
 
-neurons = pynn.Population(1, pynn.IF_cond_exp, {})
-inputs = pynn.Population(1, sim.SpikeSourcePoisson(rate=10.0))
+neurons = pynn.Population(1, pynn.IF_cond_exp, 
+            {'cm': 0.2,
+             'e_rev_I': -100.0,
+             'v_rest': -50.0,}
+          )
+neurons.record()
+inputs = pynn.Population(1, pynn.SpikeSourcePoisson, {'rate': 10.0})
 
-proj = pynn.Projection(neurons, inputs, 
-        pynn.OneToOneConnector(5.0))
+proj = pynn.Projection(inputs, neurons,
+        pynn.OneToOneConnector(weights=15.0))
 
-pynn.run(10)
+pynn.run(1000)
+
+out_spikes = np.array(neurons.getSpikes())
 
 pynn.end()
+
+plt.figure()
+plt.plot(out_spikes[:, 1], out_spikes[:, 0], '.')
+plt.savefig("output.pdf")
+
+
+
 
