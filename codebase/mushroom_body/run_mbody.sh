56974c3593f8de4163a8f6fb072317d58bde5e04
Garibaldi Pineda Garcia <gp283@inf900777.inf.susx.ac.uk>
2019-02-21 18:27:48 +0000

    trying to make synapses depress

--- codebase/mushroom_body/run_mbody.sh
+++ codebase/mushroom_body/run_mbody.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source ../venv_new/bin/activate
+
+export LD_BIND_NOW=1
+
 # nAL nKC nLH nDN scale weight_to_spike
 # for conductance neurons these seem to work
 python mbody.py 100 2500 20 100 0.025 0.0025 \
@@ -8,7 +11,7 @@ python mbody.py 100 2500 20 100 0.025 0.0025 \
     --probAL=0.2 \
     --probAL2KC=0.15 \
     --nPatternsAL=10 \
-    --nSamplesAL=1000 \
+    --nSamplesAL=6000 \
     --randomizeSamplesAL=1 \
     --renderSpikes=0 \
     --probKC2DN=0.2
